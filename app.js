4f27849c72e1bbb2167f6b09c5eac88d5d73c9ec
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-03-25 06:08:13 +0200

    made endpoints for the CRUD operations on contacts

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/error-handler");
 const dotenv = require("dotenv").config();
 const contactRouter = require("./routes/contact.router");
+const userRouter = require("./routes/user.router");
 
+connectDb();
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(errorHandler);
 
 app.use("/api/contacts", contactRouter);
+app.use("/api/users", userRouter);
 
 const PORT = 3000 || process.env.PORT;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
